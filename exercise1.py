7bc3baa70ca33923d68d22964c711937a4b830d3
ELaleman2607apo <168575087+ELaleman2607apo@users.noreply.github.com>
2025-03-17 10:38:19 -0600

    primer ejemplo

---
+++ exercise1.py
@@ -0,0 +1,12 @@
+#Comentario de una linea 
+"""
+Imprimir los datos personales del estudiante 
+"""
+
+
+print ("datos del Estudiante")
+print ("="*20)
+print ("Nombre:",end=" ")
+print ("Pablo aleman")
+print ("Apellido:",end=" ")
+print ("Aleman Solis")
